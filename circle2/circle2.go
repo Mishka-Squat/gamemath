677d4071deb36a30ee74386ce43216145d9d3e1f
Igor Karatayev <igor.cwer@gmail.com>
2026-07-21 22:25:37 +0300

    added bresenham line and circle Enum's

--- circle2/circle2.go
+++ circle2/circle2.go
@@ -1,6 +1,8 @@
 package circle2
 
 import (
+	"iter"
+
 	"github.com/Mishka-Squat/gamemath/vector2"
 	"github.com/Mishka-Squat/goex/mathex"
 )
@@ -25,4 +27,66 @@ func Make[T mathex.SignedNumber](center vector2.Of[T], radius T) Of[T] {
 		Center: center,
 		Radius: radius,
 	}
+}
+
+// BresenhamCircle calculates all grid points along the perimeter of a circle.
+// (xc, yc) is the center point, and r is the radius.
+// The yield callback processes each point. Return false to stop early.
+func Enum[T mathex.SignedNumber](c vector2.Of[T], r T) iter.Seq[vector2.Of[T]] {
+	return func(yield func(vector2.Of[T]) bool) {
+		p := vector2.Make(0, r)
+		d := 3 - (2 * r) // Initial decision parameter
+
+		// Plot the initial points on the main axes
+		if !plot8Points(c, p, yield) {
+			return
+		}
+
+		for p.Y >= p.X {
+			p.X++
+
+			// Check decision parameter to update error margin
+			if d > 0 {
+				p.Y--
+				d = d + 4*(p.X-p.Y) + 10
+			} else {
+				d = d + 4*p.X + 6
+			}
+
+			// Mirror the newly calculated point across all 8 octants
+			if !plot8Points(c, p, yield) {
+				return
+			}
+		}
+	}
+}
+
+// plot8Points mirrors a point (x, y) relative to center (xc, yc) into all 8 octants.
+func plot8Points[T mathex.SignedNumber](c, p vector2.Of[T], yield func(vector2.Of[T]) bool) bool {
+	if !yield(vector2.Make(c.X+p.X, c.Y+p.Y)) {
+		return false
+	}
+	if !yield(vector2.Make(c.X-p.X, c.Y+p.Y)) {
+		return false
+	}
+	if !yield(vector2.Make(c.X+p.X, c.Y-p.Y)) {
+		return false
+	}
+	if !yield(vector2.Make(c.X-p.X, c.Y-p.Y)) {
+		return false
+	}
+	if !yield(vector2.Make(c.X+p.Y, c.Y+p.X)) {
+		return false
+	}
+	if !yield(vector2.Make(c.X-p.Y, c.Y+p.X)) {
+		return false
+	}
+	if !yield(vector2.Make(c.X+p.Y, c.Y-p.X)) {
+		return false
+	}
+	if !yield(vector2.Make(c.X-p.Y, c.Y-p.X)) {
+		return false
+	}
+
+	return true
 }
--- line2/line2.go
+++ line2/line2.go
@@ -1,6 +1,8 @@
 package line2
 
 import (
+	"iter"
+
 	"github.com/Mishka-Squat/gamemath/vector2"
 	"github.com/Mishka-Squat/goex/mathex"
 )
@@ -27,6 +29,47 @@ func Make[T mathex.SignedNumber](a vector2.Of[T], b vector2.Of[T]) Of[T] {
 	}
 }
 
+// ABC - Calculate line's parametruc A,B,C coeffecients
 func (l Of[T]) ABC() (a T, b T, c T) {
 	return (l.B.Y - l.A.Y), (l.A.X - l.B.X), (l.B.X*l.A.Y - l.A.X*l.B.Y)
+}
+
+// Bresenham calculates all grid points along a line segment.
+// The yield callback function processes each point. Return false to stop early.
+func Enum[T mathex.SignedNumber](a, b vector2.Of[T]) iter.Seq[vector2.Of[T]] {
+	d := a.Sub(b).Abs()
+
+	sx := -1
+	if a.X < b.X {
+		sx = 1
+	}
+	sy := -1
+	if a.Y < b.Y {
+		sy = 1
+	}
+
+	err := d.X - d.Y
+
+	return func(yield func(vector2.Of[T]) bool) {
+		for p := a; p != b; {
+			// Send current coordinate to the caller
+			if !yield(p) {
+				return
+			}
+
+			e2 := 2 * err
+			if e2 > -d.Y {
+				err -= d.Y
+				p.X += T(sx)
+			}
+			if e2 < d.X {
+				err += d.X
+				p.Y += T(sy)
+			}
+		}
+	}
+}
+
+func (l Of[T]) Enum() iter.Seq[vector2.Of[T]] {
+	return Enum(l.A, l.B)
 }
